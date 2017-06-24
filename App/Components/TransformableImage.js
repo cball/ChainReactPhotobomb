ace3f02b315667da5294325ebffbe61d4aeeca9d
Chris Ball <chris@echobind.com>
2017-06-24 11:11:56 -0400

    Tidy up TransformableImage a bit

--- App/Components/TransformableImage.js
+++ App/Components/TransformableImage.js
@@ -3,59 +3,75 @@ import { Image, PanResponder, Animated, StyleSheet } from 'react-native';
 import { createResponder } from 'react-native-gesture-responder';
 
 class TransformableImage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pan: new Animated.ValueXY(),
-      scale: 1
-    };
-  }
+  state = {
+    pan: new Animated.ValueXY(),
+    scale: new Animated.Value(1.05)
+  };
 
   componentWillMount() {
     this._panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
-      onPanResponderGrant: (e, gestureState) => {
-        // Set the initial value to the current state
-        this.state.pan.setOffset({
-          x: this.state.pan.x._value,
-          y: this.state.pan.y._value
-        });
-        this.state.pan.setValue({
-          x: 0,
-          y: 0
-        });
-        // this.state.pan.setOffset({
-        //   x: this.state.pan.x._value,
-        //   y: this.state.pan.y._value
-        // });
-        // this.state.pan.setValue({ x: 0, y: 0 });
-        // Animated.spring(this.state.scale, {
-        //   toValue: 1.1,
-        //   friction: 3
-        // }).start();
+
+      onPanResponderGrant: () => {
+        this._setInitialPosition();
+        this._animateIn();
       },
 
-      // When we drag/pan the object, set the delate to the states pan position
       onPanResponderMove: Animated.event([
         null,
         {
-          dx: this.state.pan.x, // x,y are Animated.Value
+          dx: this.state.pan.x,
           dy: this.state.pan.y
         }
       ]),
 
-      onPanResponderRelease: (e, { vx, vy }) => {
-        // Flatten the offset to avoid erratic behavior
-        // this.state.pan.flattenOffset();
-        // Animated.spring(this.state.scale, { toValue: 1, friction: 3 }).start();
+      onPanResponderRelease: () => {
+        // prevent jumpiness the next time we drag
+        this.state.pan.flattenOffset();
+        this._animateBack();
       }
+    });
+
+    this._animateBack();
+  }
+
+  _animateBack() {
+    const options = {
+      toValue: 1,
+      friction: 3
+    };
+
+    Animated.spring(this.state.scale, options).start();
+  }
+
+  _animateIn() {
+    const options = {
+      toValue: 1.05,
+      friction: 3
+    };
+
+    Animated.spring(this.state.scale, options).start();
+  }
+
+  _setInitialPosition() {
+    this.state.pan.setOffset({
+      x: this.state.pan.x._value,
+      y: this.state.pan.y._value
+    });
+
+    this.state.pan.setValue({
+      x: 0,
+      y: 0
     });
   }
 
   render() {
+    const transform = this.state.pan.getTranslateTransform();
+    transform.push({ scale: this.state.scale });
+
     return (
       <Animated.View
-        style={[styles.container, this.state.pan.getLayout()]}
+        style={[styles.container, { transform }]}
         {...this._panResponder.panHandlers}
       >
         {this.props.children}
