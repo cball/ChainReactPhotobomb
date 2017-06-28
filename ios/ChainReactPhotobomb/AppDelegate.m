c6b37687846a46479bfada43208dbd0110267293
Chris Ball <chris@echobind.com>
2017-06-27 23:20:39 -0400

    android bg color

--- ios/ChainReactPhotobomb/AppDelegate.m
+++ ios/ChainReactPhotobomb/AppDelegate.m
@@ -24,7 +24,7 @@
                                                       moduleName:@"ChainReactPhotobomb"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.18 green:0.09 blue:0.26 alpha:1.0];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
